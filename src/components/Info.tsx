7b5350b34581e0c28b5b4884275ffdbb6bfaae89
ruancorrea <rhcs@ic.ufal.br>
2022-02-05 19:17:17 -0300

    whatsthat correcoes

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -11,8 +11,8 @@ export default function Info(props: InfoProps) {
                 <h2 className="text-3xl">What's that?</h2>
             </header>
             <p className="text-justify p-3">
-            It is an online tool developed to assist in the manipulation of texts that is easy to use and very useful. The tool's idealization from the context of the reactivity applied to the text input box, through the <a href="https://reactjs.org/" target={"_blank"}>React.js</a> library, is one of the points to be highlighted, because from this, we have, spontaneously, the result of each manipulation made on the entered text. 
-            The tool was developed using <a href="https://nextjs.org/" target={"_blank"}>Next.js</a> and <a href="https://tailwindcss.com/" target={"_blank"}>Tailwind CSS</a>.
+            It is an online tool developed to assist in the manipulation of texts that is easy to use and very useful. The tool's idealization from the context of the reactivity applied to the text input box, through the <a className="font-semibold" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React.js</a> library, is one of the points to be highlighted, because from this, we have, spontaneously, the result of each manipulation made on the entered text. 
+            The tool was developed using <a className="font-semibold" href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a> and <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer" className="font-semibold">Tailwind CSS</a>.
             </p>
         </div>
     )
