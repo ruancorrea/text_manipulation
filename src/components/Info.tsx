7a739d1f96b923b8812c0748bf0f1022aff27b31
ruancorrea <rhcs@ic.ufal.br>
2022-02-05 19:33:04 -0300

    whatsthat correcoes

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -11,7 +11,7 @@ export default function Info(props: InfoProps) {
                 <h2 className="text-3xl">What&apos;s that?</h2>
             </header>
             <p className="text-justify p-3">
-            It is an online tool developed to assist in the manipulation of texts that is easy to use and very useful. The tool's idealization from the context of the reactivity applied to the text input box, through the <a className="font-semibold" href="https://reactjs.org/" target="_blank" rel="noreferrer">React.js</a> library, is one of the points to be highlighted, because from this, we have, spontaneously, the result of each manipulation made on the entered text. 
+            It is an online tool developed to assist in the manipulation of texts that is easy to use and very useful. The tool&apos;s idealization from the context of the reactivity applied to the text input box, through the <a className="font-semibold" href="https://reactjs.org/" target="_blank" rel="noreferrer">React.js</a> library, is one of the points to be highlighted, because from this, we have, spontaneously, the result of each manipulation made on the entered text. 
             The tool was developed using <a className="font-semibold" href="https://nextjs.org/" target="_blank" rel="noreferrer">Next.js</a> and <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer" className="font-semibold">Tailwind CSS</a>.
             </p>
         </div>
